e677172ccc9fbd53a2cecd2c98671785721ecebb
zua <killthelast01@gmail.com>
2024-07-16 23:37:40 +0100

    chore: clean up project dir

--- src/index.ts
+++ src/index.ts
@@ -36,11 +36,9 @@ const main = async () => {
 
   const fontFiles = await getFontFiles(fontsDirPath);
   // todo: find a way to implicitly get `fontsDirPath` inside here
-  // console.log("All Font Files: ", fontFiles);
   const fontFamilies = groupFontsByFamily(fontFiles, fontsDirPath);
-  // console.log("Fams: ", fontFamilies, fontFamilies[0]?.fonts);
-  // await writeFontImports(fontsDirPath, fontFamilies, importAlias);
-  // signalSuccess(fontFamilies.map((family) => family.familyName));
+  await writeFontImports(fontsDirPath, fontFamilies, importAlias);
+  signalSuccess(fontFamilies.map((family) => family.familyName));
 };
 
 main();
