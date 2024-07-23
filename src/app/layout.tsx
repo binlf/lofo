a706e08b7531df1a333706304d0a9f71125db078
zua <killthelast01@gmail.com>
2024-07-23 23:26:50 +0100

    refactor: clean up 'generateFileContent' and 'writeImportStatement' utils

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@
 
 import { useState, useEffect } from "react";
 import { useZenState } from "zenx";
+import localfonts, { Poppins } from "../../fonts"
+// IMPORT YOUR LOCAL FONTS AS A DEFAULT EXPORT[import localfonts from ...]
+// OR AS A NAMED EXPORT[import { <font_name> } from ...]
+// YOU SHOULD PROBABLY GET RID OF THESE COMMENTS NOW
 
 export default function RootLayout() {
   return <div></div>;
