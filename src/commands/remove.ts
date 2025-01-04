9b28860e63a4816b35023ede76c671c36243b234
binlf <killthelast01@gmail.com>
2025-01-04 16:13:49 +0100

    feat: redesign CLI

--- src/commands/remove.ts
+++ src/commands/remove.ts
@@ -7,8 +7,9 @@ import path from "path";
 import { reWriteFileSync } from "../utils/write-file";
 import { isTypescriptProject } from "../utils/get-project-info";
 import { getFontsDir } from "../helpers/get-fonts-dir";
-import { folderExists, isFontFamilyDir } from "../utils/exists";
+import { folderExists, isFontFamilyDir, isFontFile } from "../utils/exists";
 import prompts, { type Choice } from "prompts";
+import pc from "picocolors";
 
 export const remove = new Command()
   .name("remove")
@@ -56,13 +57,19 @@ function removeHandler(options: { all: boolean }) {
   //   );
   //   process.exit(1);
   // }
+  const itemsInFontsDir = fs.readdirSync(fontsDirPath as string);
 
-  const fontChoices = fonts.map(
-    (font): Choice => ({
-      title: font,
-      value: font,
-    })
-  );
+  const fontChoices = fonts.map((font): Choice => {
+    const [fontFileOrDir = ""] = itemsInFontsDir.filter((item) =>
+      item.includes(font)
+    );
+
+    return {
+      // title gets displayed in the terminal
+      title: !isFontFile(fontFileOrDir) ? `${fontFileOrDir}(F)` : fontFileOrDir,
+      value: fontFileOrDir,
+    };
+  });
 
   // if (font) return removeFont(font);
 
@@ -74,12 +81,13 @@ function removeHandler(options: { all: boolean }) {
   })
     .then((res) => {
       if (!res.fontToRemove) return;
+      // return console.log(res.fontToRemove);
       removeFont(res.fontToRemove);
     })
     .catch((err) => logger.error(err));
 }
 
-const removeFont = (family?: string, flag: "single" | "all" = "single") => {
+const removeFont = (font?: string, flag: "single" | "all" = "single") => {
   const fontsDirPath = getFontsDir();
   const itemsInFontsDir = fs.readdirSync(fontsDirPath as string);
   const indexFile =
@@ -87,41 +95,42 @@ const removeFont = (family?: string, flag: "single" | "all" = "single") => {
     (isTypescriptProject() ? "index.ts" : "index.js");
   const indexFilePath = path.join(fontsDirPath!, indexFile);
   if (flag === "single") {
-    logger.info(`Removing font family: ${family}`);
-    if (!family) {
-      logger.warning("Please specify a font family to remove");
+    // logger.info(`Removing font font: ${font}`);
+    if (!font) {
+      logger.warning("Please specify a font to remove");
       process.exit(1);
     }
-    if (!itemsInFontsDir.includes(family as string)) {
+    if (!itemsInFontsDir.includes(font as string)) {
       throw new Error(
-        `Font family: ${whiteBold(family)} does not exist in ${whiteBold(
-          FONTS_DIR_NAME
-        )} directory`
+        `Font${!isFontFile(font) && " family"}: ${whiteBold(
+          font
+        )} does not exist in the ${whiteBold(FONTS_DIR_NAME)} directory`
       );
     }
-    const fontFamilyPath = path.join(fontsDirPath!, family);
-    // remove font family directory
-    fs.removeSync(fontFamilyPath);
-    const namedExport = `export const ${family} = localfont`;
-    const restFonts = fonts?.filter(
-      (fontFamily) => fontFamily !== family
-    ) as string[];
+    const fontPath = path.join(fontsDirPath!, font);
+    // remove font font directory
+    fs.removeSync(fontPath);
+    const namedExport = `export const ${font} = localfont`;
+    const restFonts = fonts?.filter((f) => f !== font) as string[];
     const defaultExport = `\nexport default {
       ${restFonts.join(", ")}
     }`;
-    // remove font family named export in index file
+    // remove  font named export in index file
     reWriteFileSync(indexFilePath, namedExport, "export", true);
-    // remove font family in default export object
+    // remove font in default export object
     reWriteFileSync(indexFilePath, defaultExport, "export");
     if (reachedSuccess) {
       updateFonts(() => restFonts);
       signalSuccess();
     }
-    return logger.success(
-      `Font family ${whiteBold(family)} was successfully removed!`
+    logger.success(`Removed font(s)`);
+    console.log(
+      `\t${pc.redBright("-")} ${!isFontFile(font) ? `${font}(F)` : font}`
     );
   }
-  logger.info(`Removing all font families`);
+
+  return;
+  // logger.info(`Removing all font families`);
   if (!itemsInFontsDir.length)
     return logger.warning(`${whiteBold(FONTS_DIR_NAME)} directory is empty`);
   let hasRemovedFontFamily = false;
