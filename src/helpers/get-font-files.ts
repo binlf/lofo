e0da47882fa1f229f208fa3cafecefc2ad3ef279
binlf <killthelast01@gmail.com>
2025-01-29 00:24:27 +0100

    chore: cleanup

--- src/helpers/get-font-files.ts
+++ src/helpers/get-font-files.ts
@@ -9,7 +9,6 @@ import { getLofoConfig } from "../utils/get-config";
 import { logger } from "../utils/logger";
 import fs from "fs-extra";
 import path from "path";
-import { getTypeface } from "../utils/get-file-names";
 
 /**
  * Returns an array of font file paths in a given directory.
@@ -18,7 +17,6 @@ import { getTypeface } from "../utils/get-file-names";
  */
 export const getFontFiles = async (fontsDirPath: string) => {
   const { fonts } = getLofoConfig();
-  // logger.info("Getting your local font files...");
   const itemsInFontsDir = fs.readdirSync(fontsDirPath);
   if (!itemsInFontsDir.length) {
     logger.warning(
@@ -28,46 +26,12 @@ export const getFontFiles = async (fontsDirPath: string) => {
     return process.exit(0);
   }
 
-  // const oldFonts: string[] = [];
-  // const newFontFilePaths: string[] = filesInFontsDir.reduce<string[]>(
-  //   (acc, file) => {
-  //     if (isFontFile(file)) {
-  //       const typeface = getTypeface(file);
-  //       if (typeface && fonts?.typefaces.includes(typeface))
-  //         oldFonts.push(typeface);
-  //       return [...acc, path.join(fontsDirPath, file)];
-  //     }
-  //     return acc;
-  //   },
-  //   []
-  // );
-
-  // const oldFontFilePaths = await oldFonts.reduce<Promise<string[]>>(
-  //   async (accPromise, font) => {
-  //     const acc = await accPromise;
-  //     const oldFontsDirPath = path.join(fontsDirPath, font);
-  //     if (doesFolderExist(oldFontsDirPath)) {
-  //       const filesInDir = await fsPromises.readdir(oldFontsDirPath);
-  //       const fontFilePaths = filesInDir
-  //         .filter((file) => isFontFile(file))
-  //         .map((f) => oldFontsDirPath + path.sep + f);
-  //       if (filesInDir.length) return [...acc, ...fontFilePaths];
-  //     }
-  //     return acc;
-  //   },
-  //   Promise.resolve([])
-  // );
-
   const fontFilePaths = itemsInFontsDir
     .reduce((acc, item, index) => {
       const itemPath = path.join(fontsDirPath, item);
       if (doesFileExist(itemPath)) {
         if (isFontFile(item)) return [...acc, itemPath];
       }
-
-      // get the 'latest' font family from st' font file in the famconfig
-      // get the 'lateily
-      // compare the birth times of the 'latest' existing font with the other birth times
 
       if (doesFolderExist(itemPath)) {
         const dirName = item;
@@ -93,17 +57,6 @@ export const getFontFiles = async (fontsDirPath: string) => {
       ];
       return timestamp - nextTimestamp;
     });
-  // if (!newFontFilePaths.length) {
-  //   logger.warning(
-  //     `Couldn't find any font files at the root of your ${FONTS_DIR_NAME} directory...\nAdd your font files to your ${FONTS_DIR_NAME} directory and run cli again...`
-  //   );
-  //   return process.exit(0);
-  // }
-  // logger.info("Found font files...");
-  // console.log(
-  //   "Font Files: ",
-  //   newFontFilePaths.map((fontFilePath) => path.basename(fontFilePath))
-  // );
 
   return fontFilePaths;
 };
